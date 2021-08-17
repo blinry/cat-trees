1148d9d7e4d8feaf5ffa27b1a9f4b81509876e70
blinry <mail@blinry.org>
2021-08-17 13:55:29 +0200

    Ah, this is how you do antialiasing!

--- script.js
+++ script.js
@@ -305,11 +305,10 @@ scene.background = new THREE.Color('gray')
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap
-renderer.antialias = true
 
 document.body.appendChild(renderer.domElement)
 
